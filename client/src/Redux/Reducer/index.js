ebe353a716f793b9bc676db5746e6a612d3b8068
jose <joseandrescolmenares02@gmail.com>
2022-05-24 12:54:18 -0300

    entorno de trabajo de redux

---
+++ client/src/Redux/Reducer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const InitialState = {
+    Usuarios: []
+}
+
+
+export default function rootReducer(state = InitialState, action) {
+switch (action.type){
+    
+
+
+    default:
+        return state;
+}
+}
+
--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import store from '../src/Redux/Store/index'
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}> 
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </BrowserRouter>,
+    </Provider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
