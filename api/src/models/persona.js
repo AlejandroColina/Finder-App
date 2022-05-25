bb5875c40abbdfe18033b9a8c65b1820c9eaddcc
AlejandroColina <alejo_9102@hotmail.com>
2022-05-25 11:08:17 -0500

    avance db

---
+++ api/src/models/persona.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+    const Persona = sequelize.define('Persona', {
+        nombres: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        apellidos: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        documento: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        telefono: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        edad: {
+            type: DataTypes.INTEGER
+        },
+    });
+
+}
