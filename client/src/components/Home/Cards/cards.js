1d68114edadaf4c424b1083c2ada0fac198a1984
jose <joseandrescolmenares02@gmail.com>
2022-05-26 15:02:38 -0300

    cards

--- client/src/components/Home/Cards/cards.js
+++ client/src/components/Home/Cards/cards.js
@@ -3,16 +3,50 @@ import person from '../../../data'
 import s from  './cards.module.css'
 export default function Cards({nombre,  imagen}) {
     return (
-        <div >
-    <div className={s.card}>
-  <div className={s.cardbordertop}>
-  </div>
-   <img  className={s.img} src={imagen} alt='img' />
-  <span>{nombre}</span>
-  <p className={s.job}>profesor</p>
-  <button className={s.button}> Click
-  </button>
-</div>
+       
+    <div className={s.container}>
+    <header>
+      <div className={s.bio}>
+        <img src="http://www.croop.cl/UI/twitter/images/up.jpg" alt="background" className={s.bg}/>
+        <div className={s.desc}>
+          <h3>descripción</h3>
+          <p>Carl Fredricksen is the protagonist in Up. He also appeared in Dug's Special Mission as a minor character.</p>
         </div>
+      </div>
+
+      <div className={s.avatarcontainer}>
+        <img src={imagen} alt="avatar" className={s.avatar}/> 
+         <p className={s.nombre}>{nombre}</p>
+        <div className={s.hover}>
+          <div className={s.icontwitter}></div>
+        </div>
+      </div>
+
+    </header>
+
+    <div className={s.content}>
+      <div className={s.data}>
+       
+        <ul>
+          <li>
+            trabajos
+            <p>pintor</p>
+          </li>
+          <li>
+           puntaje
+            <p>1,5</p>
+          </li>
+           
+        </ul>
+      </div>
+
+      <div className={s.follow}>
+        <div className={s.icontwitter}></div>mas info
+      </div>
+    </div>
+
+  </div>
+        
+  
     )
 }
