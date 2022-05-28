e63c1130be4e872413c21d20f560a14166dbd30c
AlejandroColina <alejo_9102@hotmail.com>
2022-05-28 13:07:19 -0500

    direcciones logos

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -5,8 +5,6 @@ const { Persona, Profesion, Direccion } = require("../db");
 router.use(express.json());
 
 router.get('/', async (req, res, next) => {
-
-
   try {
 
     const { profesion, nombres, promedio, genero, edad, ocupacion } = req.query;
@@ -36,31 +34,40 @@ router.get('/', async (req, res, next) => {
       }
     });
 
-
-    let filtroPersonas = objPersonas;
-    if (profesion) {
-      filtroPersonas = filtroPersonas.filter(persona => {
+    if (profesion !== undefined && profesion.length) {
+      let filtroPersonas = objPersonas.filter(persona => {
         return persona.Profesions.toLowerCase().includes(profesion.toLowerCase())
       });
-
+      !filtroPersonas.length
+        ? res.send('NO HAY CONCIDENCIAS')
+        : res.json(filtroPersonas);
     }
 
-    if (nombres) {
-      filtroPersonas = filtroPersonas.filter(persona => {
+    if (nombres !== undefined) {
+      let filtroPersonas = objPersonas.filter(persona => {
         return persona.nombres.toLowerCase().includes(nombres.toLowerCase())
       });
-
+      !filtroPersonas.length
+        ? res.send('NO HAY CONCIDENCIAS')
+        : res.json(filtroPersonas);
     }
 
-    if (genero) {
-      filtroPersonas = filtroPersonas.filter(persona => {
+    if (genero !== undefined) {
+      let filtroPersonas = objPersonas.filter(persona => {
         return persona.genero == genero
       });
-
+      !filtroPersonas.length
+        ? res.send('NO HAY CONCIDENCIAS')
+        : res.json(filtroPersonas);
     }
 
-    if (edad) {
-      filtroPersonas = filtroPersonas.filter(persona => persona.edad == edad);
+    if (edad !== undefined) {
+      let filtroPersonas = objPersonas.filter(persona => persona.edad == edad);
+      if (!filtroPersonas.length) {
+        res.send('NO HAY CONCIDENCIAS')
+      } else {
+        res.json(filtroPersonas)
+      }
     }
 
     if (promedio !== undefined) {
@@ -90,11 +97,6 @@ router.get('/', async (req, res, next) => {
 
     if (!Object.keys(req.query).length) return res.json(objPersonas);
 
-    if (promedio) {
-      filtroPersonas = filtroPersonas.filter(persona => persona.promedio >= promedio);
-    }
-
-    return res.json(filtroPersonas);
 
   } catch (error) {
     next(error)
