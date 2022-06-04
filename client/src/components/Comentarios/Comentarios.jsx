fbecd86dee1068f23452136357b72bc423274164
Camila Zupanovich <cami.zupanovich@hotmail.com>
2022-06-04 02:22:02 -0300

    cambios en detail

---
+++ client/src/components/Comentarios/Comentarios.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Comentarios({idPublicacion}){
+    return(
+        <>
+        <div>
+            <h3>Reseñas</h3>
+            <hr/>
+        </div>
+        </>
+    )
+}
