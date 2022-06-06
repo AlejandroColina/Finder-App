d7968e4e5bfdda7f3aa73ee0bfb605cfb52ebaab
AlejandroColina <alejo_9102@hotmail.com>
2022-06-06 09:12:50 -0500

    varios

--- client/src/components/perfil/Perfil.js
+++ client/src/components/perfil/Perfil.js
@@ -11,33 +11,35 @@ import Footer from "../Footer/Footer";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPefil, ValidarInfo } from "../Redux/actions/index";
+import { Link } from 'react-router-dom';
 
 const Perfil = () => {
   const { user } = useAuth0();
   const dispatch = useDispatch();
 
-  
+
   const { validar } = useSelector(state => state);
   const [perfil, setPerfil] = useState(false);
   const StatePerfil = useSelector((state) => state.perfil);
-  
+
   const handlePerfil = () => {
     setPerfil(true);
   };
-  
+
   const handleVolver = () => {
     setPerfil(false);
   };
-  
+
   useEffect(() => {
     dispatch(getPefil(user?.email));
     dispatch(ValidarInfo(user?.email));
   }, [dispatch, user?.email]);
-  
+
   return (
     <div>
       <nav className={s.nav}>
         <img className={s.logo} src={logo} alt="finder" />
+        <Link to='/home' > <button >Ir a home</button></Link>
       </nav>
       <div className={s.portada}>
         <img
@@ -48,7 +50,7 @@ const Perfil = () => {
       </div>
       <section className={s.perfil}>
         <div className={s.con_per}>
-          <img className={s.imgper} src={person[0].imagen} alt="perfil" />
+          <img className={s.imgper} src={user?.picture} alt="perfil" />
           <div className={s.nombre}>
             <h2>PERFIL</h2>
             <h1>{StatePerfil[0]?.nombres}</h1>
@@ -64,8 +66,8 @@ const Perfil = () => {
 
       <section className={s.conten}>
         <div className={s.datos}>
-          <button className={s.end} disabled={validar}>Crear publicacion</button>
-          {!validar ? <p className={s.rojo}> complete su perfil para poder hacer una apublicacion</p> : ''}
+          <Link to='/userLog' > <button className={s.end} disabled={validar}>Crear publicacion</button></Link>
+          {validar ? <p className={s.rojo}> complete su perfil para poder hacer una apublicacion</p> : ''}
           <h1 className={s.personal}>informacion personal</h1>
           <div className={s.info}>
             <h2>nombre completo :</h2>
@@ -88,17 +90,17 @@ const Perfil = () => {
             <div>
               <h2 className={s.h2}>Publicaciones</h2>
               <div className={s.centrar_publi}>
-                {StatePerfil && StatePerfil[0]?.publicaciones ? (
-                  StatePerfil[0].publicaciones.map((el) => (
+                {StatePerfil && StatePerfil[0]?.Publicacions ? (
+                  StatePerfil[0].Publicacions.map((el) => (
                     <Card_perfil
                       key={el.id}
                       precio={el.precio}
                       descripcion={el.descripcion}
                       nombre={StatePerfil[0]?.nombres}
                       imagen={StatePerfil[0]?.imagen}
-                      Profesions={StatePerfil[0]?.Profesions}
+                      Profesions={StatePerfil[0]?.Profesions[0].nombre}
                       id={el.id}
-                      logoProfesion={StatePerfil[0]?.logoProfesion}
+                      logoProfesion={StatePerfil[0]?.Profesions[0].logo}
                     />
                   ))
                 ) : (
