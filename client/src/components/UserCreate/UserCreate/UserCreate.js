147f7021d47d88e549daac87627574090e620389
AlejandroColina <alejo_9102@hotmail.com>
2022-06-08 21:31:45 -0500

    formulario publicacion

--- client/src/components/UserCreate/UserCreate/UserCreate.js
+++ client/src/components/UserCreate/UserCreate/UserCreate.js
@@ -1,123 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { useFormik } from "formik";
-import pic from "./img_fast.png";
 import axios from "axios";
 import { getEmpleosForm, getCiudades } from "../../Redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom';
 import styles from './UserCreate.module.css';
-import { Helmet } from "react-helmet";
-
-
-const validate = (values) => {
-  const errors = {};
-  if (!values.nombres) {
-    errors.nombres = "Campo Requerido";
-  } else if (values.nombres.length > 15) {
-    errors.nombres = "El máximo permitido es de 15 caractéres";
-  }
-
-  if (!values.apellidos) {
-    errors.apellidos = "Campo Requerido";
-  } else if (values.nombres.length > 15) {
-    errors.apellidos = "El máximo permitido es de 20 caractéres";
-  }
-
-  if (!values.email) {
-    errors.email = "Campo Requerido";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Dirección de correo inválida";
-  }
-
-  if (!values.documento) {
-    errors.documento = "Campo Requerido";
-  } else if (values.nombres.length > 10) {
-    errors.documento = "El máximo permitido es de 10 caractéres";
-  }
-
-  if (!values.descripcion) {
-    errors.descripcion = "Campo Requerido";
-  }
-
-  if (!values.telefono) {
-    errors.telefono = "Campo Requerido";
-  }
-
-  if (!values.direccion) {
-    errors.direccion = "Campo Requerido";
-  }
-
-  if (!values.profesion) {
-    errors.profesion = "Campo Requerido";
-  }
-
-  return errors;
-};
+import validate from './validate';
+import Swal from "sweetalert2";
 
 export default function UserCreate() {
   const { user } = useAuth0();
   const dispatch = useDispatch();
-
-  const [image, setImage] = useState([]);
-  const [selected, setSelected] = useState('');
-  const [city, setCity] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const formik = useFormik({
-    initialValues: {
-      titulo: "",
-      descripcion: "",
-      precio: "",
-    },
-    validate,
-    onSubmit: (values) => {
-      alert(JSON(values, null, 2));
-    },
-  });
 
   useEffect(() => {
     dispatch(getEmpleosForm());
     dispatch(getCiudades());
   }, [dispatch]);
 
-  const uploadImage = async (e) => {
-    const files = e.target.files;
-    const data = new FormData();
-    data.append("file", files[0]);
-    data.append("upload_preset", "preset");
-    setLoading(true);
-
-    const res = await fetch(
-      "https://api.cloudinary.com/v1_1/userfiles/image/upload",
-      {
-        method: "POST",
-        body: data,
-      }
-    );
-
-    const file = await res.json();
-
-    setImage(() => {
-      if (file.secure_url.length) return [...image, file.secure_url]
-      return image
-    });
-    setLoading(false);
-  };
-
-  const delImgSelection = (urlImg) => {
-    setImage(image.filter(url => url !== urlImg))
-  }
-
+  const [image, setImage] = useState([]);
+  const [selected, setSelected] = useState('');
+  const [city, setCity] = useState(0);
   const empleos = useSelector((state) => state.empleosForm);
   const ciudades = useSelector(state => state.ciudades);
-  const [empleosSelected, setEmpleosSelected] = useState({});
-
-  const selectChange = (e) => {
-    let id = e.target.id;
-    let value = e.target.value;
-    setEmpleosSelected({ ...empleosSelected, [id]: value });
-  };
-
+  const [errors, setError] = useState({});
   const [input, setInput] = useState({
     titulo: "",
     descripcion: "",
@@ -125,32 +30,24 @@ export default function UserCreate() {
     multimedia: [],
   });
 
-  let toSend = {
-    titulo: input.titulo,
-    descripcion: input.descripcion,
-    ProfesionId: selected,
-    precio: input.precio,
-    multimedia: image,
-    email: user?.email,
-    ciudad: city,
-  }
-
   const handleChange = (e) => {
-    let name = e.target.name;
-    let value = e.target.value;
-
-    setInput({
-      ...input,
-      [name]:
-        name === "precio"
-          ? !isNaN(parseInt(value))
-            ? parseInt(value)
-            : (value = "")
-          : value,
+    let { name, value } = e.target;
+    setInput((prevState) => {
+      let newState = {
+        ...prevState,
+        [name]: name === 'precio'
+          ? !isNaN(parseInt(value)) ? parseInt(value) : value = ''
+          : value
+      }
+      setError(validate(newState));
+      return newState;
     });
   };
 
-  function handleSubmit(e) {
+  const handleChange1 = (e) => { setSelected(e.target.value) }
+  const handleChange2 = (e) => { setCity(e.target.value) }
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     setInput({
       titulo: "",
@@ -161,20 +58,55 @@ export default function UserCreate() {
     setSelected('')
     setImage([])
     setCity(0)
+    setError({})
 
-    axios
-      .post("http://localhost:3001/users/crear", { toSend })
-      .then((res) => {
-        alert("Publicación creada");
+    axios.post("http://localhost:3001/users/crear", { toSend })
+      .then(() => {
+        Swal.fire("Perfecto!",
+          'Ya puedes ver tu postulación en nuestra app. Para más acciones búscala en tu perfil',
+          'success');
       })
       .catch((error) => {
+        Swal.fire("No se pudo crear la publicación",
+          'Hubo un problema al publicar tu solicitud. Por favor contacta a administración',
+          'error');
         console.log(error);
-        alert("No se pudo crear la publicación");
       });
-  }
+  };
 
-  const handleChange1 = (e) => { setSelected(e.target.value) }
-  const handleChange2 = (e) => { setCity(e.target.value) }
+  const uploadImage = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "preset");
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/userfiles/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+    const file = await res.json();
+    setImage(() => {
+      if (file.secure_url.length) return [...image, file.secure_url]
+      return image
+    });
+  };
+
+  const delImgSelection = (urlImg) => {
+    setImage(image.filter(url => url !== urlImg))
+  };
+
+  let toSend = {
+    titulo: input.titulo,
+    descripcion: input.descripcion,
+    ProfesionId: selected,
+    precio: input.precio,
+    multimedia: image,
+    email: user?.email,
+    ciudad: city,
+  }
 
   return (
     <section className={styles.container}>
@@ -196,58 +128,48 @@ export default function UserCreate() {
         </div>
 
         <form className={styles.formulario} onSubmit={handleSubmit}>
-
           <div className={styles.formulario2}>
             <h1 className={styles.log4}> Crea tu publicación</h1>
 
             <label htmlFor="Título de la publicación"></label>
-            {formik.touched.apellidos && formik.errors.edad ? (
-              <div className={styles.required}>{formik.errors.edad}</div>
-            ) : null}
             <input
-              className={styles.inputs}
+              className={errors?.titulo ? styles.inputs_fail : styles.inputs}
               id="titulo"
               placeholder="Un título para la publicación..."
               name="titulo"
               type="text"
               onChange={handleChange}
               value={input.titulo}
-              onBlur={formik.handleBlur}
             />
-
+            <p >{errors?.titulo}</p>
 
             <label htmlFor="descripcion"></label>
-            {formik.touched.descripcion && formik.errors.descripcion ? (
-              <div className={styles.required}>{formik.errors.descripcion}</div>
-            ) : null}
             <textarea
-              className={styles.textarea}
+              className={errors.descripcion ? styles.textarea_fail : styles.textarea}
               id="descripcion"
               placeholder="Descripcion..."
               name="descripcion"
               type="textarea"
               onChange={handleChange}
               value={input.descripcion}
-              onBlur={formik.handleBlur}
             />
+            <p>{errors?.descripcion}</p>
 
             <label htmlFor="Precio"></label>
-            {formik.touched.apellidos && formik.errors.edad ? (
-              <div className={styles.required}>{formik.errors.edad}</div>
-            ) : null}
             <input
-              className={styles.inputs}
+              className={errors?.precio ? styles.inputs_fail : styles.inputs}
               id="precio"
               placeholder="Precio solicitado para el servicio..."
               name="precio"
               type="number"
               onChange={handleChange}
               value={input.precio}
-              onBlur={formik.handleBlur}
             />
+            <p >{errors?.precio}</p>
 
             <div className={styles.div_select}>
               <select className={styles.selects} value={city} onChange={handleChange2}>
+                <option>CIUDAD</option>
                 {ciudades &&
                   ciudades.map((el, index) => (
                     <option key={index + 'DIR'}
@@ -261,6 +183,7 @@ export default function UserCreate() {
               <div>
                 <div>
                   <select className={styles.selects} value={selected} onChange={handleChange1}>
+                    <option>PROFESION</option>
                     {empleos &&
                       empleos.map((el, id) => (
                         <option key={id + 'DR'}
@@ -304,7 +227,7 @@ export default function UserCreate() {
                   && city
                   ? styles.bnt_file_none
                   : styles.btn_file
-              }
+              } D
             >
               Crear publicación
             </button>
@@ -323,11 +246,6 @@ export default function UserCreate() {
           })
         }
       </div>
-
-
     </section >
   );
 }
-
-// select con  tipos  de profesiones
-// con axios llamar a la ruta de empleos
