5846bf96b2f91b56a94dc7692df173223f69989c
jose <joseandrescolmenares02@gmail.com>
2022-06-08 22:02:51 -0300

    ya casi

--- client/src/components/perfil/favoritos/Favoritos.js
+++ client/src/components/perfil/favoritos/Favoritos.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Cards from './Card'
+import s from './Fvorito.module.css'
 
+const Favorito = () => {
+  const favorito = useSelector((state) => state.favorito);
+ 
+  return (
+    <div className={s.conte}>
+      {favorito &&
+        favorito.map((el) => (
+          <Cards
+            nombre={el.nombre}
+            imagen={el.imagen}
+            descripcion={el.descripcion}
+            Profesions={el.Profesions}
+            id={el.id}
+            logoProfesion={el.logoProfesion}
+          />
+        ))}
+    </div>
+  );
+};
 
-const  Favorito = () =>{
-    return (
-        <div>
-       en este momento no tienes favoritos
-        </div>
-    )
-}
-
-export default Favorito
+export default Favorito;
