1db6ee8df85119a752c6f884491dfaaced5fd53c
AlejandroColina <alejo_9102@hotmail.com>
2022-06-11 09:40:07 -0500

    link from home to landing

--- api/src/app.js
+++ api/src/app.js
@@ -12,8 +12,19 @@ const pregunta = require('./routes/pregunta');
 const baneo = require('./routes/admin');
 const notificaciones = require('./routes/notificaciones');
 const emailToConfirm = require('./routes/emailToConfirm');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 server.use(cors());
+
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 server.use('/users', users);
 server.use('/publicaciones', publicaciones);
@@ -28,6 +39,13 @@ server.use('/notificaciones', notificaciones);
 server.use('/email', emailToConfirm);
 server.get('*', (req, res) => {
     res.send('<h1>Página NO encontrada en FINDER. 😡🥶</h1>')
+});
+
+server.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
 });
 
 module.exports = server;
