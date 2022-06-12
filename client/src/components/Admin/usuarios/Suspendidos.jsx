ffa7855d6534055dce5a89643d809551cdd7f60c
AlejandroColina <alejo_9102@hotmail.com>
2022-06-11 20:29:57 -0500

    acciones admin

--- client/src/components/Admin/usuarios/Suspendidos.jsx
+++ client/src/components/Admin/usuarios/Suspendidos.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import {useDispatch} from "react-redux";
-import { desbanear} from "../../Redux/actions";
+import { useDispatch } from "react-redux";
+import { desbanear, sendDesBaneo } from "../../Redux/actions";
 import as from './Config.module.css';
 
-export default function Suspendidos({suspendidos}){
+export default function Suspendidos({ suspendidos }) {
 
     const dispatch = useDispatch();
 
@@ -11,24 +11,25 @@ export default function Suspendidos({suspendidos}){
 
     const debanear = false
     const handleDesbanear = (id) => {
-      dispatch(desbanear(id, debanear));
+        dispatch(desbanear(id, debanear));
+        dispatch(sendDesBaneo(id));
     }
 
-    return(
+    return (
         <div className={as.gridCol}>
             <div className={as.gridItem}>
                 <div>ID</div>
                 <div>MAIL</div>
                 <div>TELEFONO</div>
             </div>
-            {suspendidos? suspendidos.map(s=>
-            <div className={as.gridItem} key={s.id}>
-              <div className={as.btnAc} onClick={() => handleDesbanear(s.id)}>activar</div>
-                <div className={as.gridDivs}>{s.id}</div>
-                <div className={as.gridDivs}>{s.email}</div>
-                <div className={as.gridDivs}>{s.telefono}</div>
-            </div>)
-            :<div>no hay usuarios suspendidos</div>}
+            {suspendidos ? suspendidos.map(s =>
+                <div className={as.gridItem} key={s.id}>
+                    <div className={as.btnAc} onClick={() => handleDesbanear(s.id)}>activar</div>
+                    <div className={as.gridDivs}>{s.id}</div>
+                    <div className={as.gridDivs}>{s.email}</div>
+                    <div className={as.gridDivs}>{s.telefono}</div>
+                </div>)
+                : <div>no hay usuarios suspendidos</div>}
         </div>
     )
 }
