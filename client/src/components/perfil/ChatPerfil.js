58f0dd244553866652c01524eeb56bfea55898bb
GiulianoB94 <83374374+GiulianoB94@users.noreply.github.com>
2022-06-16 04:05:10 -0300

    css-Chat

--- client/src/components/perfil/ChatPerfil.js
+++ client/src/components/perfil/ChatPerfil.js
@@ -4,31 +4,38 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import "./ChatPerfil.css";
 
-const MisChats = () =>{
-    const dispatch = useDispatch();
-    const StatePerfil = useSelector((state) => state.perfil);
-    const losChats = StatePerfil[0].chats
-  console.log(losChats)
-    const { user } = useAuth0();
+const MisChats = () => {
+  const dispatch = useDispatch();
+  const StatePerfil = useSelector((state) => state.perfil);
+  const losChats = StatePerfil[0].chats;
+  console.log(losChats);
+  const { user } = useAuth0();
 
-    useEffect(() => {
-        dispatch(getPefil(user?.email));
-      }, [dispatch, user?.email]);
+  useEffect(() => {
+    dispatch(getPefil(user?.email));
+  }, [dispatch, user?.email]);
 
-
-    return(
-         <div>
-            <h1>Mis Chats:</h1>
-            { StatePerfil[0].chats && StatePerfil[0].chats.map((e)=>{
-              return <ul>
-                <Link to={`/chat/${e.chat}`}><button>{e.name}</button></Link>
-              </ul>
-              
-            })   
-            }
-         </div>
-    )
-}
+  return (
+    <div>
+      <h1 className="chatTittle">Mis Chats</h1>
+      <div className="contChat">
+        {StatePerfil[0].chats &&
+          StatePerfil[0].chats.map((e) => {
+            return (
+              <div className="listChat">
+                <ul>
+                  <Link to={`/chat/${e.chat}`}>
+                    <button className="buttonChat">{e.name}</button>
+                  </Link>
+                </ul>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
 
 export default MisChats;
